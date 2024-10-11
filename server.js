0c993df0d9dd8146dd14ce45e733b7ea137ce817
Nis3D <103100755+Nis3D@users.noreply.github.com>
2024-10-11 13:29:11 +0800

    Modifying index and css file and placing them in public directory. Adding path to connect it.

--- server.js
+++ server.js
@@ -1,50 +1,53 @@
 const express = require('express');
 const app = express();
+const path =require('path');
+
 require('dotenv').config();
 const cors = require('cors');
 
 
-// Set the port for the application
-const PORT = process.env.PORT || 3000;
-
 // Enable CORS for cross-origin requests
 app.use(cors());
 
-app.use(express.static('public'));
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname+"/views/index.html");
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-    res.json({ greeting: 'hello API' });
-  });
-
   
 //middleware to handle JSON requets
 app.use(express.json());
 
-// // Route to handle the /api/whoami request
-// app.get('/api/whoami', (req, res) => {
+// Route to handle the /api/whoami request
+app.get('/api/whoami', (req, res) => {
 
-//     // Getting the IP address from the request
-//     const ipaddress = req.ip || req.headers['x-forwarded-for'] || req.remoteAddress;
+    // Getting the IP address from the request
+    const ipaddress = req.ip || req.headers['x-forwarded-for'] || req.remoteAddress;
 
-//     // Getting the language from the 'Accept-Language' header
-//     const language = req.headers['accept-language'].split(',')[0];
+    // Getting the language from the 'Accept-Language' header
+    const language = req.headers['accept-language'] || "Unknown";
 
-//     // Getting the user-agent from the 'User-Agent' header
-//     const software = req.headers['user-agent'];
+    // Getting the user-agent from the 'User-Agent' header
+    const software = req.headers['user-agent'] || "Unknown";
 
-//     // Respond with a JSON object
-//     res.json({
-//          ipaddress,
-//          language,
-//          software 
-//     });
-// });
+    // Respond with a JSON object
+    res.json({
+         ipaddress: ipaddress,
+         language: language,
+         software: software 
+    });
+});
 
+
+// Set the port for the application
+const PORT = process.env.PORT || 3000;
 
 // Start the server
 app.listen(PORT, () => {
